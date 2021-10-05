e6e6a1158465f2e13ad53f802561f09dd06e5bd9
Sym3008 <s30@libero.it>
2021-10-05 13:57:39 +0200

    recupero classe libri controller

--- src/main/java/it/biblioteca/www/Biblioteca/controller/LibriController.java
+++ src/main/java/it/biblioteca/www/Biblioteca/controller/LibriController.java
@@ -1,4 +1,47 @@
 package it.biblioteca.www.Biblioteca.controller;
 
+import com.sun.istack.NotNull;
+import it.biblioteca.www.Biblioteca.model.Libri;
+import it.biblioteca.www.Biblioteca.service.LibriService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class LibriController {
+
+    @Autowired
+    private LibriService libriService;
+
+    @GetMapping("/get-libri")
+    public List<Libri> getLibri(){
+        return libriService.getLibri();
+    }
+
+    @GetMapping("/get-libri/{id}")
+    public Libri getLibroById(@PathVariable("id") Integer id){
+        return libriService.getLibroById(id);
+    }
+
+    @PostMapping("/save-libri")
+    public void saveLibri(@RequestBody @NotNull List<Libri> libri){
+        libriService.saveOrUpdateLibri(libri);
+    }
+
+    @PutMapping("/update-libri")
+    public void updateLibri(@RequestBody @NotNull List<Libri> libri){
+        libriService.saveOrUpdateLibri(libri);
+    }
+
+    @DeleteMapping("/canella-libri")
+    public void deleteLibri(@RequestBody @NotNull List<Libri> libri){
+        libriService.deleteLibri(libri);
+    }
+
+    @DeleteMapping("/canella-libro/{id}")
+    public void deleteLibroById(@PathVariable("id") Integer id){
+        libriService.deleteLibroById(id);
+    }
 }
