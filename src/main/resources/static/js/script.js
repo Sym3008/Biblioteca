3012bf941fe982d62acc3947241f65b279e27866
VincenzoMorelli <vincenzo.morelli@outlook.com>
2021-10-11 15:56:43 +0200

    registra

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,31 +1,69 @@
-// window.addEventListener("load", function (Event){
-//
-//     let form=document.querySelector("#login")
-//
-//     form.addEventListener("submit",function (e){
-//         e.preventDefault()
-//         let id=document.querySelector("#Email")
-//         let urlApi= "http://localhost:8080/api/get-anagrafica/"+id.value
-//
-//         console.log(id.value)
-//         console.log(urlApi)
-//         fetch(urlApi,
-//         {method:"GET"
-//         }).then(function (response){
-//             return response.json()
-//         }).then(function (data){
-//             console.log(data)
-//         })
-//     })
-// })
+window.addEventListener('load', function(event){
+    let form = document.querySelector('#login')
+    form.addEventListener('submit', function (event){
+        formValido = true;
+        text=document.querySelectorAll('.textInput.richiesto');
+        text.forEach(function (elT,ind,ar){
+            if (elT.value === ""){
+                console.log("-> " + elT.id + " = NON E' PRESENTE!!");
+                formValido = false;
+                InErr(elT,elT.id);
+            }else {
+                console.log("-> " + elT.id + " = " + elT.value);
+                TogErr(elT);
+            }
+        })
 
-window.addEventListener("load", function (Event){
+        function InErr(elemento,text){
+            if(elemento!== null){
+                let mess= elemento.closest('.boxIn').querySelector('.messErr');
+                mess.innerHTML="---"+text+" obbligatorio---";
 
-    let form=document.querySelector("#login")
+            }
+        }
+        function TogErr(elemento){
+            if(elemento!== null){
+                let mess= elemento.closest('.boxIn').querySelector('.messErr');
+                mess.innerHTML="";
+            }
+        }
 
-    form.addEventListener("submit",function (e){
-        let id=document.querySelector("#Email")
-        let urlApi= "http://localhost:63342/Biblioteca/templates/show/AutoriShow.html?id="+id.value
-        window.open(urlApi);
+        if(formValido === false){
+            console.log("ERRORE!!!");
+            event.preventDefault();
+        } else {
+            inserisciRecord();
+            event.preventDefault();
+        }
     })
 })
+
+function inserisciRecord(){
+    let E=document.querySelector('#Email');
+    let P=document.querySelector('#Password');
+
+    let newRecord ={
+        email:E.value,
+        password:P.value};
+
+    console.log(newRecord);
+
+    let urlApi= "http://localhost:8080/api/save-login";
+
+    fetch(urlApi, {
+        method: "POST",
+        headers:{
+            "content-type":"application/json",
+            "Accept":"*/*",
+            "Accept-Encoding":"gzip,deflate,br",
+            "Connection":"keep-live"
+        },
+        body: JSON.stringify(newRecord),
+    }).then(function (response){
+        console.log("record inserito");
+        alert("Login inserita correttamente")
+        // return response.json()
+    }).then(data => {
+        console.log('Success:', data);
+    });
+}
